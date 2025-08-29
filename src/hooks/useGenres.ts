9738e4f92d100ffe518f4c21783e3f1c2f7126e4
Yugal Khanal <yugalkhanal5@gmail.com>
2025-08-29 19:03:06 +0100

    ship static data for genres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,15 +1,15 @@
-import useData from "./useData";
+import genres from "@/data/genres";
 
 export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-  count: number;
+	id: number;
+	name: string;
+	image_background: string;
+	count: number;
 }
 
 const useGenres = () => {
-  const { data, error, isLoading } = useData<Genre>("/genres");
-  return { data, error, isLoading };
+	// const { data, error, isLoading } = useData<Genre>("/genres");
+	return { data: genres, error: null, isLoading: false };
 };
 
 export default useGenres;
