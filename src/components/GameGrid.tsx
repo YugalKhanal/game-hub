bccea7bcde50e87c35b62ac533123e0af2c0de5c
Yugal Khanal <yugalkhanal5@gmail.com>
2025-09-05 17:07:28 +0100

    add custom grays and refactor gamegrid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,36 +6,37 @@ import GameCardContainer from "./GameCardContainer";
 import type { GameQuery } from "@/App";
 
 interface Props {
-	gameQuery: GameQuery;
+  gameQuery: GameQuery;
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
-	const { data, error, isLoading } = useGames(gameQuery);
-	const skeletons = [1, 2, 3, 4, 5, 6];
+  const { data, error, isLoading } = useGames(gameQuery);
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-	return (
-		<>
-			{error && <Text>{error}</Text>}
-			<SimpleGrid
-				columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-				padding={"10px"}
-				gap="10px"
-				columnGap={6}
-			>
-				{isLoading &&
-					skeletons.map((skeleton) => (
-						<GameCardContainer key={skeleton}>
-							<GameCardSkeleton key={skeleton} />
-						</GameCardContainer>
-					))}
-				{data.map((game) => (
-					<GameCardContainer key={game.id}>
-						<GameCard key={game.id} game={game} />
-					</GameCardContainer>
-				))}
-			</SimpleGrid>
-		</>
-	);
+  if (error) return <Text>{error}</Text>;
+
+  return (
+    <>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding={"10px"}
+        gap="10px"
+        columnGap={6}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
+        {data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
+        ))}
+      </SimpleGrid>
+    </>
+  );
 };
 
 export default GameGrid;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "./components/ui/provider.tsx";
 import "./index.css";
 import App from "./App.tsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { system } from "./theme.ts";
+import { Provider } from "./components/ui/provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <ChakraProvider value={system}>
+        <App />
+      </ChakraProvider>
     </Provider>
   </StrictMode>,
 );
