f0b03ecb40aab98b80e641044db2055bfadfd029
Yugal Khanal <yugalkhanal5@gmail.com>
2025-09-05 17:17:36 +0100

    fixed games count name in Genre interface in useGenres.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Grid, GridItem, HStack } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -10,71 +10,71 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-	genre: Genre | null;
-	platform: Platform | null;
-	sortOrder: string;
-	searchText: string;
+  genre: Genre | null;
+  platform: Platform | null;
+  sortOrder: string;
+  searchText: string;
 }
 
 function App() {
-	const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
 
-	return (
-		<>
-			<Grid
-				templateAreas={{
-					base: `"nav" "main"`,
-					lg: `"nav nav" "aside main" `,
-				}}
-				templateColumns={{
-					base: "1fr",
-					lg: "200px 1fr",
-				}}
-			>
-				<GridItem area="nav">
-					<NavBar
-						onSearch={(searchText) =>
-							setGameQuery({ ...gameQuery, searchText })
-						}
-					/>
-				</GridItem>
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main" `,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "200px 1fr",
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
+            }
+          />
+        </GridItem>
 
-				<GridItem
-					area="aside"
-					display={{ base: "none", lg: "block" }}
-					paddingX={5}
-				>
-					<GenreList
-						onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-						selectedGenre={gameQuery.genre}
-					/>
-				</GridItem>
+        <GridItem
+          area="aside"
+          display={{ base: "none", lg: "block" }}
+          paddingX={5}
+        >
+          <GenreList
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            selectedGenre={gameQuery.genre}
+          />
+        </GridItem>
 
-				<GridItem area="main">
-					<Box paddingLeft={2}>
-						<GameHeading gameQuery={gameQuery} />
-						<Flex marginBottom={5}>
-							<Box marginRight={5}>
-								<PlatformSelector
-									onSelectedPlatform={(platform) =>
-										setGameQuery({ ...gameQuery, platform })
-									}
-									selectedPlatform={gameQuery.platform}
-								/>
-							</Box>
-							<SortSelector
-								sortOrder={gameQuery.sortOrder}
-								onSelectSortOrder={(sortOrder) =>
-									setGameQuery({ ...gameQuery, sortOrder })
-								}
-							/>
-						</Flex>
-					</Box>
-					<GameGrid gameQuery={gameQuery} />
-				</GridItem>
-			</Grid>
-		</>
-	);
+        <GridItem area="main">
+          <Box paddingLeft={2}>
+            <GameHeading gameQuery={gameQuery} />
+            <Flex marginBottom={5}>
+              <Box marginRight={5}>
+                <PlatformSelector
+                  onSelectedPlatform={(platform) =>
+                    setGameQuery({ ...gameQuery, platform })
+                  }
+                  selectedPlatform={gameQuery.platform}
+                />
+              </Box>
+              <SortSelector
+                sortOrder={gameQuery.sortOrder}
+                onSelectSortOrder={(sortOrder) =>
+                  setGameQuery({ ...gameQuery, sortOrder })
+                }
+              />
+            </Flex>
+          </Box>
+          <GameGrid gameQuery={gameQuery} />
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,57 +1,57 @@
 import useGenres, { type Genre } from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
 import {
-	Button,
-	Heading,
-	HStack,
-	Image,
-	List,
-	Spinner,
+  Button,
+  Heading,
+  HStack,
+  Image,
+  List,
+  Spinner,
 } from "@chakra-ui/react";
 
 interface Props {
-	onSelectGenre: (genre: Genre) => void;
-	selectedGenre: Genre | null;
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
 const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
-	const { data, error, isLoading } = useGenres();
+  const { data, error, isLoading } = useGenres();
 
-	if (error) return null;
+  if (error) return null;
 
-	if (isLoading) return <Spinner />;
+  if (isLoading) return <Spinner />;
 
-	return (
-		<>
-			<Heading fontSize={"2xl"} marginBottom={3}>
-				Genres
-			</Heading>
-			<List.Root variant={"plain"}>
-				{data.map((genre) => (
-					<List.Item key={genre.id} paddingY={"5px"}>
-						<HStack>
-							<Image
-								boxSize={"32px"}
-								borderRadius={8}
-								objectFit={"cover"}
-								src={getCroppedImageUrl(genre.image_background)}
-							/>
-							<Button
-								whiteSpace={"normal"}
-								textAlign={"left"}
-								fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-								onClick={() => onSelectGenre(genre)}
-								fontSize={"lg"}
-								variant={"ghost"}
-							>
-								{genre.name}
-							</Button>
-						</HStack>
-					</List.Item>
-				))}
-			</List.Root>
-		</>
-	);
+  return (
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
+      <List.Root variant={"plain"}>
+        {data.map((genre) => (
+          <List.Item key={genre.id} paddingY={"5px"}>
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={8}
+                objectFit={"cover"}
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                onClick={() => onSelectGenre(genre)}
+                fontSize={"lg"}
+                variant={"ghost"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </List.Item>
+        ))}
+      </List.Root>
+    </>
+  );
 };
 
 export default GenreList;
